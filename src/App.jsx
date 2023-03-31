bb1c252572db7a8769c07591f92eb7a9dd757c7e
AmbrishShukla <104611402+AmbrishShukla@users.noreply.github.com>
2023-03-31 23:18:41 +0530

    final changes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import RegisterAndLoginForm from "./RegisterAndLoginForm";
+import React from "react";
+import axios from "axios";
+import { UserContextProvider } from "./UserContext";
+import Routes from "./Routes";
+
+function App() {
+
+  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.withCredentials = true;
+  return (
+    <UserContextProvider>
+      <Routes></Routes>
+    </UserContextProvider>
+  );
+}
+
+export default App;
